b45e07123b418c5e1f3e8acb7df6b740b228aebd
Mateusz Kretowski <matkretkurs@gmail.com>
2023-05-03 15:11:15 +0200

    Change prepared to publication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 // import routes
 const seatsRoutes = require('./routes/seats.routes');
@@ -8,16 +9,21 @@ const concertsRoutes = require('./routes/concerts.routes');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build'))); // Serve static files from the React app
 app.use(express.urlencoded({ extended: false })); //x-www-form-urlencoded
 app.use(express.json()); //form-data
 app.use('/', seatsRoutes); // add seats routes to server
 app.use('/', concertsRoutes); // add concerts routes to server
 app.use('/', testimonialsRoutes); // add testimonials routes to server
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
